4166bf4d05d2a2d08dec9c1f48033c89b566e0dd
Stefan Sonski <s.sonski@googlemail.com>
2012-10-22 10:02:58 +0200

    Added answer for flexibility of the pattern

--- src/Operation.java
+++ src/Operation.java
@@ -1,3 +1,10 @@
+/**
+ * Flexibility:
+ * It is easy to add new operation to this pattern.
+ * Therefor only {@link #setOperation()} has to be called in the constructor of a new Operation.
+ * Also {@link #evaluate()} has to be implemented depending on the new operation.
+ * @author Stefan Sonski
+ */
 abstract public class Operation extends Expression {
 	private Expression left;
 	private Expression right;
